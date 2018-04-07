3b8e2ab40d43021daff4fd107f3e9dfe1b4acada
sanei <saneimousavi93@gmail.com>
2018-04-07 23:02:24 +0430

    ClientProfileSearchResult edited

--- fah-script.js
+++ fah-script.js
@@ -2,3 +2,7 @@ function changeColorOnSelect(obj) {
     obj.style.backgroundColor = "palegreen";
 }
 
+function changeCardColor(obj) {
+    obj.parentElement.style.backgroundColor = "palegreen";
+}
+
